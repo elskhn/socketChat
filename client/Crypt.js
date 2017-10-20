f7d299b5f9f96714a8bae5a612965be716808c81
Abdullah <abdullahkhan0503@gmail.com>
2017-10-20 14:07:06 +0000

    formatted Crypt.js
    just beautified the CryptoJS client-side file

--- client/Crypt.js
+++ client/Crypt.js
@@ -1,17 +1,21 @@
 /* global Crypt, CryptoJS*/
-Crypt = function (passphrase) {
+Crypt = function(passphrase) {
     "use strict";
     var pass = passphrase;
     var CryptoJSAesJson = {
-        parse: function (jsonStr) {
+        parse: function(jsonStr) {
             var j = JSON.parse(jsonStr);
-            var cipherParams = CryptoJS.lib.CipherParams.create({ciphertext: CryptoJS.enc.Base64.parse(j.ct)});
+            var cipherParams = CryptoJS.lib.CipherParams.create({
+                ciphertext: CryptoJS.enc.Base64.parse(j.ct)
+            });
             if (j.iv) cipherParams.iv = CryptoJS.enc.Hex.parse(j.iv);
             if (j.s) cipherParams.salt = CryptoJS.enc.Hex.parse(j.s);
             return cipherParams;
         },
-        stringify: function (cipherParams) {
-            var j = {ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64)};
+        stringify: function(cipherParams) {
+            var j = {
+                ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64)
+            };
             if (cipherParams.iv) j.iv = cipherParams.iv.toString();
             if (cipherParams.salt) j.s = cipherParams.salt.toString();
             return JSON.stringify(j);
@@ -19,11 +23,15 @@ Crypt = function (passphrase) {
     };
 
     return {
-        decrypt: function (data) {
-            return JSON.parse(CryptoJS.AES.decrypt(data, pass, {format: CryptoJSAesJson}).toString(CryptoJS.enc.Utf8));
+        decrypt: function(data) {
+            return JSON.parse(CryptoJS.AES.decrypt(data, pass, {
+                format: CryptoJSAesJson
+            }).toString(CryptoJS.enc.Utf8));
         },
-        encrypt: function (data) {
-            return CryptoJS.AES.encrypt(JSON.stringify(data), pass, {format: CryptoJSAesJson}).toString();
+        encrypt: function(data) {
+            return CryptoJS.AES.encrypt(JSON.stringify(data), pass, {
+                format: CryptoJSAesJson
+            }).toString();
         }
     };
 };
