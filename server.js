3af0240e3b9f6cc718f85189bc71cf43b9a1c9c8
Abdullah <abdullahkhan0503@gmail.com>
2017-10-20 14:08:57 +0000

    formatted server.js
    beautification of the main server-side file (server.js)

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 var Crypt = require("g-crypt"),
-	passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
-	crypter = Crypt(passphrase);
+    passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
+    crypter = Crypt(passphrase);
 
 // use 'client' folder to GET client-side files
 app.use(express.static('client'));
@@ -18,56 +18,56 @@ var connections = [];
 
 server.listen(process.env.PORT);
 
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 //when someone connects
-io.sockets.on('connection', function (socket) {
-	connections.push(socket);
-	console.log('New connection detected! (Total connections: %s)', connections.length);
-	socket.on('counter', function (data) {
-		var decriptedData = crypter.decrypt(data);
-		//prints sent/recieved encryptred messages in console
-		//console.log(socket.username + ": " + data);
-		setTimeout(function () {
-			//console.log("counter status: " + decriptedData.id);
-			decriptedData.id++;
-			socket.emit('counter', crypter.encrypt(decriptedData));
-			//console.log(crypter.encrypt(decriptedData));
-		}, 1000);
-	});
-	//when a client has disconnected
-	socket.on('disconnect', function (data) {
-		users.splice(users.indexOf(socket.username), 1);
-		updateUserNames();
-		connections.splice(connections.indexOf(socket), 1);
-		console.log("A client has disconnected (Total connections now: %s)", connections.length);
-	});
+io.sockets.on('connection', function(socket) {
+    connections.push(socket);
+    console.log('New connection detected! (Total connections: %s)', connections.length);
+    socket.on('counter', function(data) {
+        var decriptedData = crypter.decrypt(data);
+        //prints sent/recieved encryptred messages in console
+        //console.log(socket.username + ": " + data);
+        setTimeout(function() {
+            //console.log("counter status: " + decriptedData.id);
+            decriptedData.id++;
+            socket.emit('counter', crypter.encrypt(decriptedData));
+            //console.log(crypter.encrypt(decriptedData));
+        }, 1000);
+    });
+    //when a client has disconnected
+    socket.on('disconnect', function(data) {
+        users.splice(users.indexOf(socket.username), 1);
+        updateUserNames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("A client has disconnected (Total connections now: %s)", connections.length);
+    });
 
-	//send message
-	socket.on('sendMsg', function (data) {
-		if (data.trim() != "" && data.length <= 27) {
-			io.sockets.emit('newMsg', {
-				msg: data,
-				user: socket.username
-			});
-		}
-		console.log(socket.username + ": " + crypter.encrypt(data));
-		//console.log(socket.username + ": " + crypter.decrypt(data) + " (decrypted)");
-	});
+    //send message
+    socket.on('sendMsg', function(data) {
+        if (data.trim() != "" && data.length <= 27) {
+            io.sockets.emit('newMsg', {
+                msg: data,
+                user: socket.username
+            });
+        }
+        console.log(socket.username + ": " + crypter.encrypt(data));
+        //console.log(socket.username + ": " + crypter.decrypt(data) + " (decrypted)");
+    });
 
-	//new user
-	socket.on('newUser', function (data, callback) {
-		callback(true);
-		socket.username = data;
-		users.push(socket.username);
-		updateUserNames();
-	});
+    //new user
+    socket.on('newUser', function(data, callback) {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUserNames();
+    });
 
-	//update user names function
-	function updateUserNames() {
-		io.sockets.emit('getUsers', users);
-	}
+    //update user names function
+    function updateUserNames() {
+        io.sockets.emit('getUsers', users);
+    }
 });
 console.log("server is live!");
