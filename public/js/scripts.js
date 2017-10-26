a9cf066ee7a163aff5ef730ef573b8c0194afc6e
Abdullah <abdullahkhan0503@gmail.com>
2017-10-26 19:35:10 +0000

    switch to arrow-notation + username char limit
    use arrow-function notation for most callback functions (jQuery).
    add a character limit for usernames.
    some minor UX/UI updates.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
 /* global $, io, Crypt, i*/
-$(document).ready(function () {
+$(document).ready(() => {
 	var socket = io.connect();
 	//var socket = io.connect('http://khanabdullah.com:8080');
 	//message screen
@@ -17,13 +17,13 @@ $(document).ready(function () {
 	var passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
 		crypter = Crypt(passphrase),
 		id = 0;
-	socket.on('connect', function () {
+	socket.on('connect', () => {
 		//console.log("connected! Let's start the counter with: " + id);
 		socket.emit('counter', crypter.encrypt({
 			id: id
 		}));
 	});
-	socket.on('counter', function (data) {
+	socket.on('counter', data => {
 		var decriptedData = crypter.decrypt(data);
 		//console.log("counter status: " + decriptedData.id);
 		socket.emit('counter', crypter.encrypt({
@@ -32,8 +32,8 @@ $(document).ready(function () {
 		//console.log(socket.username + ": " + crypter.decrypt(data) + " (decrypted)");
 	});
 
-	$messageForm.on("submit", function (e) {
-		e.preventDefault();
+	$messageForm.on("submit", event => {
+		event.preventDefault();
 		if ($message.val().trim() != "" && $message.val().length <= 27) {
 			socket.emit('sendMsg', $message.val());
 			$message.val('');
@@ -42,11 +42,11 @@ $(document).ready(function () {
 		}
 	});
 
-	$loginForm.on("submit", function (e) {
-		e.preventDefault();
-		if ($username.val() != "") {
+	$loginForm.on("submit", event => {
+		event.preventDefault();
+		if ($username.val() != "" && $username.val().length <= 8) {
 			window.sessionUser = $username.val();
-			socket.emit('newUser', $username.val(), function (data) {
+			socket.emit('newUser', $username.val(), data => {
 				if (data) {
 					$loginForm.fadeOut("slow");
 					$('#intro-container').fadeOut("slow");
@@ -57,22 +57,24 @@ $(document).ready(function () {
 				}
 			});
 		} else {
-			alert("Please enter an actual username. Remember, everyone will see this name.");
+			alert("Please enter an actual username. Remember, everyone will see this name. Your username must be less than 9 characters.");
 		}
 		$username.val('');
 	});
 	
 	// when a new message is transmitted from the server
-	socket.on('newMsg', function (data) {
+	socket.on('newMsg', data => {
+		//if(!$('#chat').has($('.message')).length) {
+			$('#empty-chat-text').fadeOut(500);
+		//}
 		// check if the user sending the message is this sessions' user or someone else...
 		$chat.append('<div class="well message '+ (window.sessionUser == data.user ? "mine" : "not-mine" ) +'"><strong>' + data.user + ': </strong>' + data.msg + ' </div>');
 		// add a line break after every message for aesthetic purposes
 		$chat.append('<br class="chat-line-break">');
 	});
 
-	socket.on('getUsers', function (data) {
+	socket.on('getUsers', data => {
 		var html = '';
-
 
 		for (i = 0; i < data.length; i++) {
 		    //let id = i + 1;
@@ -82,5 +84,5 @@ $(document).ready(function () {
         // console.log(data[0]);
 		$users.html(html);
 	});
-
+	
 });
