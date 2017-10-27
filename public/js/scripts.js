2b71e47f47143327703f89b546f51e93282fb5c9
Abdullah <abdullahkhan0503@gmail.com>
2017-10-27 18:06:28 +0000

    refactor JS
    upgrade JS to include ES6 features (client & server side)!
    tidy up some stuff, too.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,35 +1,34 @@
 /* global $, io, Crypt, i*/
 $(document).ready(() => {
-	var socket = io.connect();
-	//var socket = io.connect('http://khanabdullah.com:8080');
+	
+	const socket = io.connect();
 	//message screen
-	var $messageArea = $('#messageArea');
-	var $messageForm = $('#messageForm');
-	var $message = $('#message');
-	var $users = $('#users');
+	const $messageArea = $('#messageArea'),
+		  $messageForm = $('#messageForm'),
+		  $message = $('#message'),
+		  $users = $('#users');
 	//chat screen
-	var $chat = $('#chat');
+	const $chat = $('#chat');
 	//login area
-	var $loginForm = $('#loginForm');
-	var $loginArea = $('#loginArea');
-	var $username = $('#username');
+	const $loginForm = $('#loginForm'),
+		  $loginArea = $('#loginArea'),
+		  $username = $('#username');
 
-	var passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
+	let passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
 		crypter = Crypt(passphrase),
 		id = 0;
+		
 	socket.on('connect', () => {
-		//console.log("connected! Let's start the counter with: " + id);
 		socket.emit('counter', crypter.encrypt({
 			id: id
 		}));
 	});
+	
 	socket.on('counter', data => {
 		var decriptedData = crypter.decrypt(data);
-		//console.log("counter status: " + decriptedData.id);
 		socket.emit('counter', crypter.encrypt({
 			id: decriptedData.id
 		}));
-		//console.log(socket.username + ": " + crypter.decrypt(data) + " (decrypted)");
 	});
 
 	$messageForm.on("submit", event => {
@@ -64,9 +63,7 @@ $(document).ready(() => {
 	
 	// when a new message is transmitted from the server
 	socket.on('newMsg', data => {
-		//if(!$('#chat').has($('.message')).length) {
-			$('#empty-chat-text').fadeOut(500);
-		//}
+		$('#empty-chat-text').fadeOut(500);
 		// check if the user sending the message is this sessions' user or someone else...
 		$chat.append('<div class="well message '+ (window.sessionUser == data.user ? "mine" : "not-mine" ) +'"><strong>' + data.user + ': </strong>' + data.msg + ' </div>');
 		// add a line break after every message for aesthetic purposes
@@ -77,11 +74,8 @@ $(document).ready(() => {
 		var html = '';
 
 		for (i = 0; i < data.length; i++) {
-		    //let id = i + 1;
 			html += '<li id="' + id + '" class="list-group-item"><i class="fa fa-circle" style="color: #40f23a;"></i> ' + data[i] + ' </li>';
-			//html += '<li id="' + id + '" class="list-group-item"><i class="fa fa-circle" style="color: #40f23a;"></i> ' + data[i] + (id == 1 ? "   (you)" : "") + ' </li>';
 		}
-        // console.log(data[0]);
 		$users.html(html);
 	});
 	
--- server.js
+++ server.js
@@ -1,44 +1,41 @@
 //require express and make an express application
-var express = require('express');
+const express = require('express');
 var app = express();
 
-var server = require('http').createServer(app);
+const server = require('http').createServer(app);
 //require socket.io and listen to server
-var io = require('socket.io').listen(server);
+const io = require('socket.io').listen(server);
 
-var Crypt = require("g-crypt"),
+let Crypt = require("g-crypt"),
     passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
     crypter = Crypt(passphrase);
 
 // use 'client' folder to GET client-side files
 app.use(express.static('public'));
 
-var users = [];
-var connections = [];
+let users = [];
+let connections = [];
 
 server.listen(process.env.PORT);
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 //when someone connects
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', socket => {
     connections.push(socket);
     console.log('New connection detected! (Total connections: %s)', connections.length);
-    socket.on('counter', function(data) {
-        var decriptedData = crypter.decrypt(data);
-        // prints sent/recieved encryptred messages in server log for demo purposes
-        //console.log(socket.username + ": " + data);
-        setTimeout(function() {
-            //console.log("counter status: " + decriptedData.id);
+    socket.on('counter', data => {
+        let decriptedData = crypter.decrypt(data);
+        setTimeout(() => {
             decriptedData.id++;
             socket.emit('counter', crypter.encrypt(decriptedData));
-            //console.log(crypter.encrypt(decriptedData));
         }, 1000);
     });
+    
     //when a client has disconnected
-    socket.on('disconnect', function(data) {
+    socket.on('disconnect', data => {
         users.splice(users.indexOf(socket.username), 1);
         updateUserNames();
         connections.splice(connections.indexOf(socket), 1);
@@ -46,19 +43,19 @@ io.sockets.on('connection', function(socket) {
     });
 
     //send message
-    socket.on('sendMsg', function(data) {
+    socket.on('sendMsg', data => {
         if (data.trim() != "" && data.length <= 27) {
             io.sockets.emit('newMsg', {
                 msg: data,
                 user: socket.username
             });
         }
+        // prints sent/recieved encryptred messages in server log for demo purposes
         console.log(socket.username + ": " + crypter.encrypt(data));
-        //console.log(socket.username + ": " + crypter.decrypt(data) + " (decrypted)");
     });
 
     //new user
-    socket.on('newUser', function(data, callback) {
+    socket.on('newUser', (data, callback) => {
         if(data.length <= 8){
             callback(true);
             socket.username = data;
