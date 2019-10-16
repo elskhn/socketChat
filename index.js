c25276167ff40310ca8b10e988a4ab4d86e63b61
Abdullah Khan <contact@khanabdullah.com>
2019-10-15 23:12:15 -0600

    fix POST and login functionality
    fix form POST requests and handle authoriziation (redirect to login)
    
    Signed-off-by: Abdullah Khan <contact@khanabdullah.com>

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
-const express = require('express'),
-      compression = require('compression')
+const express = require("express"),
+      session = require("express-session"),
+      compression = require("compression"),
+      bodyParser = require('body-parser')
 
 let app = express()
-const http = require("http").createServer(app)
+const server = require("http").createServer(app)
+const io = require('socket.io').listen(server)
 const routes = require("./routes/main")
 
 // one month cache period for static files
 let cacheTime = 30 * 24 * 60 * 60 * 1000
 // compress app responses (before sending to client)
 app.use(compression())
-// use the 'public' directory to serve static files (and set cache time)
+// use the "public" directory to serve static files (and set cache time)
 app.use(express.static(__dirname + "/public", {maxAge: cacheTime}))
 // set express view engine to render Pug
 app.set("view engine", "pug")
+// url encoding
+app.use(express.urlencoded({extended: true}))
 
+function checkLoggedIn(req, res, next) {
+  if (req.session == null){
+    res.redirect('/login')
+  }
+  else{
+    next()
+  }
+}
+
+// redirect to login if not logged in yet
+app.get('*', (req, res, next) => {
+  if(req.url.indexOf('/login') == -1) {
+    checkLoggedIn(req, res, next)
+  }
+  else{
+    next()
+  }
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// create routes for all the pages
 for (let pages in routes){
   app.use(pages, routes[pages])
 }
 
-http.listen(4000, () => {
-  console.log("server is live on port 3030")
+// io.sockets.on('connection', socket => {
+//   socket.on('login', data => {
+    
+//   })
+// })
+
+server.listen(4000, () => {
+  console.log("server is live on port 4000")
 })
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,21 @@
 const express = require("express"),
-      router = express.Router()
+      router = express.Router(),
+      bodyParser = require('body-parser')
+
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: true }))
 
 router.get("/", (request, response) => {
   response.render("home", {title: "socketChat",
                       author: "Abdullah F. Khan",
-                      description: "Welcome to socketChat, a secure messaging application created by Abdullah Khan"});
-});
+                      description: "Welcome to socketChat, a secure messaging application created by Abdullah Khan"})
+})
+
+router.post("/", (request, response) => {
+  request.session = true;
+  response.redirect("/")
+  console.log(request.body.username)
+  console.log(request.body.roomCode)
+})
 
 module.exports = router;
